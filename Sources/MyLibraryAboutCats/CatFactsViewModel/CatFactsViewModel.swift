8ebb2577596818d5eb8a843cbd170ca25ebba138
vako03 <valeri.mekhashishvili97@gmail.com>
2024-05-02 11:48:53 +0400

    initial commit

--- Sources/MyLibraryAboutCats/CatFactsViewModel/CatFactsViewModel.swift
+++ Sources/MyLibraryAboutCats/CatFactsViewModel/CatFactsViewModel.swift
@@ -5,24 +5,23 @@
 //  Created by vako on 02.05.24.
 //
 
+
 import Foundation
 
 public class CatFactsViewModel {
-    public init() {}
-
-    public var facts: [CatFact] = [] 
     private let api = CatFactsAPI()
-
+    public private(set) var facts: [CatFact] = []
+    
     public func fetchFacts(completion: @escaping () -> Void) {
         api.fetchFacts { [weak self] result, error in
             guard let self = self else { return }
-
+            
             if let error = error {
                 print("Error fetching facts: \(error.localizedDescription)")
                 completion()
                 return
             }
-
+            
             if let result = result {
                 self.facts = result
                 completion()
@@ -30,4 +29,3 @@ public class CatFactsViewModel {
         }
     }
 }
-
